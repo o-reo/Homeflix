1120cd32fff93502ed03ac63c5fc90017c52a134
Emmanuel Ruaud <ruaudemmanuel@hotmail.fr>
2018-10-16 18:06:33 +0200

    Login fix in progress

--- client/src/app/auth/auth.component.ts
+++ client/src/app/auth/auth.component.ts
@@ -15,17 +15,18 @@ export class AuthComponent implements OnInit {
   password: string;
   user: SocialUser;
 
-  constructor(private authSimpleService: AuthsimpleService, private router: Router, private authService: AuthService, private http: HttpClient) {
+  constructor(private authSimpleService: AuthsimpleService, private router: Router,
+              private authService: AuthService, private http: HttpClient) {
   }
 
   ngOnInit() {
     this.authService.authState.subscribe((user) => {
       this.user = user;
     });
-    var parts = window.location.search.substr(1).split('&');
-    var $_GET = {};
-    for (var i = 0; i < parts.length; i++) {
-      var temp = parts[i].split('=');
+    const parts = window.location.search.substr(1).split('&');
+    const $_GET = {};
+    for (let i = 0; i < parts.length; i++) {
+      const temp = parts[i].split('=');
       $_GET[decodeURIComponent(temp[0])] = decodeURIComponent(temp[1]);
     }
     if ('code' in $_GET && (('isLog' in localStorage) === false
@@ -39,7 +40,7 @@ export class AuthComponent implements OnInit {
     console.log(this.user);
     this.authSimpleService.goAuthWithGoogle(this.user)
       .subscribe(response => {
-        if (response.success == true) {
+        if (response.success === true) {
           localStorage.setItem('isLog', 'true');
           localStorage.setItem('token', response.token);
           this.authSimpleService.myUser = response.user;
@@ -51,37 +52,43 @@ export class AuthComponent implements OnInit {
       });
   }
 
+  redirectTo42(): void {
+    window.location.href = 'https://api.intra.42.fr/oauth/authorize?' +
+      'client_id=5ce22ae173d5c328029ee542afc8fe162935f4698642f91afe77a747dac61523&' +
+      'redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fauth%2F&response_type=code';
+  }
+
   signInWith42(code: string): void {
-    const auth = {
-      grant_type: 'authorization_code',
-      client_id: '5ce22ae173d5c328029ee542afc8fe162935f4698642f91afe77a747dac61523',
-      client_secret: 'b82a3dbdec7522f57b01cfc3e17cee54ddbce81021555ddacb130a13a6ee8dbc',
-      code: code,
-      redirect_uri: 'http://localhost:4200/auth/'
-    };
-    console.log('CODE: ', code);
-    this.http.post<any>('https://api.intra.42.fr/oauth/token', auth)
-      .subscribe(response => {
-        let headers = new HttpHeaders();
-        headers = headers.append('Authorization', 'Bearer ' + response.access_token);
-        console.log('TOKEN: ', response.access_token);
-        this.http.get<any>('https://api.intra.42.fr/v2/me', {headers: headers})
-          .subscribe(user => {
-            this.authSimpleService.goAuthWith42(user)
-              .subscribe(res => {
-                console.log('RESPONSE: ', res);
-                if (res.success == true) {
-                  localStorage.setItem('isLog', 'true');
-                  localStorage.setItem('token', res.token);
-                  this.authSimpleService.myUser = res.user;
-                  console.log('resp: ', res.user);
-                  this.router.navigate(['torrents']);
-                } else {
-                  localStorage.removeItem('isLog');
-                }
-              });
-          });
-      });
+  const auth = {
+    grant_type: 'authorization_code',
+    client_id: '5ce22ae173d5c328029ee542afc8fe162935f4698642f91afe77a747dac61523',
+    client_secret: 'b82a3dbdec7522f57b01cfc3e17cee54ddbce81021555ddacb130a13a6ee8dbc',
+    code: code,
+    redirect_uri: 'http://localhost:4200/auth/'
+  };
+  console.log('CODE: ', code);
+  this.http.post<any>('https://api.intra.42.fr/oauth/token', auth)
+    .subscribe(response => {
+      let headers = new HttpHeaders();
+      headers = headers.append('Authorization', 'Bearer ' + response.access_token);
+      console.log('TOKEN: ', response.access_token);
+      this.http.get<any>('https://api.intra.42.fr/v2/me', {headers: headers})
+        .subscribe(user => {
+          this.authSimpleService.goAuthWith42(user)
+            .subscribe(res => {
+              console.log('RESPONSE: ', res);
+              if (res.success === true) {
+                localStorage.setItem('isLog', 'true');
+                localStorage.setItem('token', res.token);
+                this.authSimpleService.myUser = res.user;
+                console.log('resp: ', res.user);
+                this.router.navigate(['torrents']);
+              } else {
+                localStorage.removeItem('isLog');
+              }
+            });
+        });
+    });
   }
 
   goAuth() {
@@ -89,9 +96,11 @@ export class AuthComponent implements OnInit {
       username: this.username,
       password: this.password
     };
+    console.log('User form credentials:', newAuth);
     this.authSimpleService.goAuth(newAuth)
       .subscribe(response => {
-        if (response.success == true) {
+        console.log('Loggin response', response);
+        if (response.success === true) {
           localStorage.setItem('isLog', 'true');
           localStorage.setItem('token', response.token);
           this.authSimpleService.myUser = response.user;
@@ -99,7 +108,9 @@ export class AuthComponent implements OnInit {
           this.router.navigate(['torrents']);
         } else {
           localStorage.removeItem('isLog');
+          console.log('Log status:', 'failure');
         }
+        console.log('Log status:', localStorage);
       });
   }
 
