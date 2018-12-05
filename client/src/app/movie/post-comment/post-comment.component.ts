6fe4fea16d24221af7256f7f3e34089e2901cb08
Emmanuel Ruaud <ruaudemmanuel@hotmail.fr>
2018-12-05 18:25:50 +0100

    Fixed comment overflow, character count is now limited

--- client/src/app/movie/post-comment/post-comment.component.ts
+++ client/src/app/movie/post-comment/post-comment.component.ts
@@ -1,8 +1,8 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
 import {Input} from '@angular/core';
 import {CommentService} from '../../comment.service';
 import 'froala-editor/js/froala_editor.pkgd.min.js';
+import {MatSnackBar} from '@angular/material';
 
 declare var $: any;
 
@@ -13,14 +13,12 @@ declare var $: any;
 })
 export class PostCommentComponent implements OnInit {
 
-
-  formGroup = new FormGroup({
-    form1: new FormControl()
-  });
   @Input() id_movie: string;
 
-  constructor(private commentService: CommentService) {
+  constructor(private commentService: CommentService, public snackBar: MatSnackBar) {
   }
+
+  public editorContent = '';
 
   public options: Object = {
     placeholderText: '...',
@@ -32,21 +30,28 @@ export class PostCommentComponent implements OnInit {
     toolbarButtonsXS: ['bold', 'italic', 'underline', 'color', 'clearFormatting'],
     toolbarButtonsSM: ['bold', 'italic', 'underline', 'color', 'clearFormatting'],
     toolbarButtonsMD: ['bold', 'italic', 'underline', 'color', 'clearFormatting'],
+    charCounterMax: 80,
+    // enter: $.FroalaEditor.ENTER_DIV
   };
 
   ngOnInit() {
   }
 
-  onSubmit() {
-    if (this.formGroup.value.form1) {
+  SubmitEditor() {
+    if (this.editorContent.length > 80 || this.editorContent.split(/<p>/).length > 3) {
+      this.snackBar.open('Your message is too long', 'X', {
+        duration: 3000,
+      });
+    }
+    else if (this.editorContent) {
       const newComment = {
         id_movie: this.id_movie,
-        content: this.formGroup.value.form1
+        content: this.editorContent
       };
       this.commentService.postComment(newComment)
         .subscribe(msg => {
           this.commentService.comments.push(msg);
-          this.formGroup.reset();
+          this.editorContent = '';
         });
     }
   }
